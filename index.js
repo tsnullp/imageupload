4ea53bf17cf1ebd1d56c54220c23d55dbc2f2e8b
tsnullp <tsnullp@gmail.com>
2022-11-07 15:02:23 +0900

    aa

--- index.js
+++ index.js
@@ -79,7 +79,10 @@ app.post('/upload-multi', async(req, res) => {
           !fs.existsSync(UPLOAD) && fs.mkdirSync(UPLOAD)
           !fs.existsSync(path.join(UPLOAD, today)) && fs.mkdirSync(path.join(UPLOAD, today))
           let i = 0
+          console.log("base64strs", typeof base64strs)
+          console.log("base64strs", base64strs.length)
           for(const item of req.body.base64strs) {
+            console.log("item", item)
             try {
               let randomStr = Math.random().toString(36).substring(2, 12);
               let fileName = path.join(today, `${randomStr}.jpg`)
